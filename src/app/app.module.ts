810e6ef07c71570656062e80466063873265fb7c
SangoDesire <analysteprodez@gmail.com>
2023-01-19 19:16:30 +0100

    signup page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,21 @@ import { AccueilComponent } from './components/accueil/accueil.component';
 import { LoginComponent } from './components/login/login.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { SignupComponent } from './components/signup/signup.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     AccueilComponent,
     LoginComponent,
-    DashboardComponent
+    DashboardComponent,
+    SignupComponent,
+
+
+
+
   ],
   imports: [
     BrowserModule,
