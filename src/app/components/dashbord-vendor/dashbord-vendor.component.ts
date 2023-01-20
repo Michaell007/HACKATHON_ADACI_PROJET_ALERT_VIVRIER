4ad17aa0bcb9756fea277fcdd80940a1469dd4e9
Nadia <nadiagueu@gmail.com>
2023-01-20 09:45:39 +0000

    dashboard vendeuse

---
+++ src/app/components/dashbord-vendor/dashbord-vendor.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dashbord-vendor',
+  templateUrl: './dashbord-vendor.component.html',
+  styleUrls: ['./dashbord-vendor.component.css']
+})
+export class DashbordVendorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
